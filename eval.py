39571eb7c4c352c25a64c43587ab05552490606d
Jason Chen (ads) <cyhy462702086@gmail.com>
2024-11-10 18:20:29 -0500

    save number of boxes per image to csv

--- eval.py
+++ eval.py
@@ -1,11 +1,23 @@
 from ultralytics import YOLO
 
-model = YOLO('best_float32.tflite')
+model = YOLO('best_float32.tflite', task="detect")
 
 outputs = model.predict('test/leaf', save=True, imgsz=1024, conf=0.2)
 
 
 len(results[0].boxes)
 
-for output in ouputs:
-	print("there are", len(output.boxes), "leaves detected.")
+
+import pandas as pd
+
+df = pd.DataFrame({
+    'image_name': pd.Series(dtype='str'),
+    'num_boxes': pd.Series(dtype='int')
+})
+
+for output in outputs:
+	image_name: str = output.path.split("/")[-1]
+	num_boxes: int = len(output.boxes)
+	df.loc[len(df)] = [image_name, num_boxes]
+
+df.to_csv('output.csv', index=False)
