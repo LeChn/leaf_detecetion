ab05221cca05e3c681628b1863d33eebe489b7cc
Jason Chen (ads) <cyhy462702086@gmail.com>
2024-11-10 17:55:06 -0500

    print number of boxes detected

--- eval.py
+++ eval.py
@@ -2,4 +2,10 @@ from ultralytics import YOLO
 
 model = YOLO('best_float32.tflite')
 
-model.predict('test/leaf', save=True, imgsz=1024, conf=0.2)
+outputs = model.predict('test/leaf', save=True, imgsz=1024, conf=0.2)
+
+
+len(results[0].boxes)
+
+for output in ouputs:
+	print("there are", len(output.boxes), "leaves detected.")
